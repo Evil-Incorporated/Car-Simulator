cb4cde978228946d1a4971771aae246e2954ec00
Evil-Incorporated <rajarshi.banerjee47@gmail.com>
2019-10-01 03:23:09 +0530

    Fixed Initialization

--- car_simulator/__init__.py
+++ car_simulator/__init__.py
@@ -2,5 +2,5 @@ from gym.envs.registration import register
 
 register(
     id='car_simulator-v0',
-    entry_point='car_simulator.envs:CarSimulatorEnv',
+    entry_point='car_simulator.envs:Simulator',
 )
